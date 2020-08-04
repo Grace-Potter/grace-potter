b98777c095dead9df0292a65c7ceeb52f826dd98
Mike Koshakow <mzkosh@gmail.com>
2020-08-04 15:54:39 -0400

    seed users

--- seed.js
+++ seed.js
@@ -1,3 +1,5 @@
+const {green, red} = require('chalk')
+const loremIpsum = require('lorem-ipsum').loremIpsum
 const {db, Product, User} = require('./server/db')
 
 products = [
@@ -74,6 +76,8 @@ products = [
   }
 ]
 
+users = []
+
 const seed = async () => {
   try {
     await db.sync({force: true})
@@ -82,7 +86,34 @@ const seed = async () => {
       const instance = Product.create(item)
       return instance
     })
+
+    for (let i = 0; i < 6; i++) {
+      const user = await User.create({
+        email: `${loremIpsum({count: 1, units: 'word'})}@fsa.com`,
+        password: password,
+        firstName: loremIpsum({count: 1, units: 'word'}),
+        lastName: loremIpsum({count: 1, units: 'word'})
+      })
+      users.push(user)
+    }
   } catch (err) {
     console.error(err)
   }
+}
+
+module.exports = seed
+// If this module is being required from another module, then we just export the
+// function, to be used as necessary. But it will run right away if the module
+// is executed directly (e.g. `node seed.js` or `npm run seed`)
+if (require.main === module) {
+  seed()
+    .then(() => {
+      console.log(green('Seeding success!'))
+      db.close()
+    })
+    .catch(err => {
+      console.error(red('Oh noes! Something went wrong!'))
+      console.error(err)
+      db.close()
+    })
 }
