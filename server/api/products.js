11f01a17af9527c496b3809ba3944b804652ee57
Jonathan Albert <JonathanAlbert0115@gmail.com>
2020-08-04 15:18:14 -0400

    Defined user, order, order_item, product models. created express route to findAll products and it working in Postman

---
+++ server/api/products.js
@@ -0,0 +1,22 @@
+const router = require('express').Router()
+const {Product} = require('../db/models')
+module.exports = router
+
+//JA: Currently retrieves all items even if the item is out of stock. Will have to decide if this is expected.""
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll({})
+    res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const products = await Product.findByPk(req.params.id)
+    res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
---
+++ server/db/models/product.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING
+    // allowNull: false,
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  price: {
+    type: Sequelize.FLOAT
+    // allowNull: false,
+    // validate: {
+    //   isEmpty: false,
+    // },
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  },
+  imageUrl: {
+    type: Sequelize.STRING
+  }
+})
+
+module.exports = Product
