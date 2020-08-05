0b8efd797a0a7295743959b2eb28ff3c10b83014
Gregory Millare <gmillare4@gmail.com>
2020-08-05 10:43:35 -0500

    singleItemView can get state from its reducer

--- client/components/singleItemView.js
+++ client/components/singleItemView.js
@@ -2,7 +2,12 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 const singleItemView = props => {
+  console.log('props.state', props.state)
   return <div>hello single Item</div>
 }
 
-export default singleItemView
+const mapStateToProps = reducerState => ({
+  state: reducerState.singleItem.testState
+})
+
+export default connect(mapStateToProps)(singleItemView)
---
+++ client/store/singleItem.js
@@ -0,0 +1,9 @@
+const initialState = {
+  testState: ['state1']
+}
+/**
+ * REDUCER
+ */
+export default function(state = initialState, action) {
+  return state
+}
