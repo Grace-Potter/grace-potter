8d926962398e56d86da46b9b7ec76a22c1be8465
Mike Koshakow <mzkosh@gmail.com>
2020-08-06 14:47:37 -0400

    nav buttons

--- client/components/AdminPortal.js
+++ client/components/AdminPortal.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 class AdminPortal extends React.Component {
   render() {
-    return <h1>Admin Portal</h1>
+    return (
+      <nav>
+        <h1>Admin Portal</h1>
+        <Link to="/admin-portal/manageproducts">Manage Producs</Link>
+        <Link to="/admin-portal/manageusers">Manage Users</Link>
+      </nav>
+    )
   }
 }
 
