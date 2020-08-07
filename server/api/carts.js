bde43d54c8359b5cca687c6a197ca05e514216bb
Jonathan Albert <JonathanAlbert0115@gmail.com>
2020-08-07 16:50:00 -0400

    Finalized add to cart, modify quantity in cart, and remove from cart express routes

--- server/api/carts.js
+++ server/api/carts.js
@@ -26,7 +26,7 @@ router.get('/:userId/currentCart/', async (req, res, next) => {
     const order = await Order.findAll({
       include: {all: true},
       where: {
-        id: req.params.userId,
+        userId: req.params.userId,
         status: 'InProgress'
       }
     })
@@ -36,7 +36,35 @@ router.get('/:userId/currentCart/', async (req, res, next) => {
   }
 })
 
-router.post('/:userId/currentCart/:productId', async (req, res, next) => {
+router.get('/:userId/currentCart/checkout', async (req, res, next) => {
+  try {
+    //current cart in progress
+    const order = await Order.findAll({
+      include: {
+        all: true
+      },
+      where: {
+        userId: req.params.userId,
+        status: 'InProgress'
+      }
+    })
+
+    //make sure that you're not trying to check out more than what we have in inventory
+    let outOfStockItems = []
+
+    order[0].dataValues.products.forEach(item => {
+      if (item.quantity < item.orderItem.quantity) {
+        outOfStockItems.push(item)
+      }
+    })
+
+    res.json(outOfStockItems)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:userId/currentCart/checkout', async (req, res, next) => {
   try {
     //current cart in progress
     const order = await Order.findAll({
@@ -46,12 +74,132 @@ router.post('/:userId/currentCart/:productId', async (req, res, next) => {
       }
     })
 
-    const addToCart = await OrderItem.create({
-      productId: req.params.productId,
-      orderId: order[0].id
+    //create array of all items to be checked out
+    const checkoutItems = Array.from(
+      await OrderItem.findAll({
+        where: {
+          orderId: order[0].id
+        }
+      })
+    )
+
+    //update all inventory items
+    checkoutItems.forEach(async item => {
+      let product = await Product.findAll({
+        where: {
+          id: item.productId
+        }
+      })
+
+      await Product.update(
+        {
+          quantity: product[0].quantity - item.quantity
+        },
+        {
+          where: {
+            id: item.productId
+          }
+        }
+      )
     })
-    res.json(addToCart)
+
+    //changes cart status to Complete
+    // await Order.update(
+    //   {
+    //     status: 'Complete',
+    //   },
+    //   {
+    //     where: {
+    //       id: order[0].id,
+    //     },
+    //   }
+    // )
+
+    //create a new cart that's status InProgress
+    // await Order.create({
+    //   userId: req.params.userId,
+    // })
+
+    res.sendStatus(201)
   } catch (err) {
     next(err)
   }
 })
+
+router.post(
+  '/:userId/currentCart/product/:productId',
+  async (req, res, next) => {
+    try {
+      //current cart in progress
+      const order = await Order.findAll({
+        where: {
+          userId: req.params.userId,
+          status: 'InProgress'
+        }
+      })
+
+      await OrderItem.create({
+        productId: req.params.productId,
+        orderId: order[0].id
+      })
+      res.sendStatus(201)
+    } catch (err) {
+      next(err)
+    }
+  }
+)
+
+router.put(
+  '/:userId/currentCart/product/:productId/quantity/:quantity',
+  async (req, res, next) => {
+    try {
+      //current cart in progress
+      const order = await Order.findAll({
+        where: {
+          userId: req.params.userId,
+          status: 'InProgress'
+        }
+      })
+
+      await OrderItem.update(
+        {
+          quantity: req.params.quantity
+        },
+        {
+          where: {
+            orderId: order[0].id,
+            productId: req.params.productId
+          }
+        }
+      )
+      res.sendStatus(202)
+    } catch (err) {
+      next(err)
+    }
+  }
+)
+
+router.delete(
+  '/:userId/currentCart/product/:productId',
+  async (req, res, next) => {
+    try {
+      const order = await Order.findAll({
+        where: {
+          userId: req.params.userId,
+          status: 'InProgress'
+        }
+      })
+
+      await OrderItem.destroy({
+        where: {
+          orderId: order[0].id,
+          productId: req.params.productId
+        }
+      })
+
+      res.sendStatus(200)
+    } catch (err) {
+      next(err)
+    }
+  }
+)
