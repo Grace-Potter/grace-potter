7ed33db4c193649ace466f6d2af5280a8bd0ab63
Mike Koshakow <mzkosh@gmail.com>
2020-08-08 14:50:36 -0400

    create api route for getting single user data

--- client/components/UserCard.js
+++ client/components/UserCard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-const UserCard = ({email}) => {
+const UserCard = ({email, id}) => {
   return (
     <l1>
-      <h3>{email}</h3>
+      <Link to={`/admin-portal/manageusers/${id}`}>
+        <h3>{email}</h3>
+      </Link>
     </l1>
   )
 }
