23220c37e0bfe7ff871ce59ba42be27dc28259fb
Mike Koshakow <mzkosh@gmail.com>
2020-08-09 14:24:01 -0400

    check for admin in users api route

--- server/api/users.js
+++ server/api/users.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 const {User} = require('../db/models')
 const {Order} = require('../db/models')
+const {handle404, checkAdmin} = require('../../util/server')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+router.get('/', checkAdmin, async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
@@ -18,13 +19,14 @@ router.get('/', async (req, res, next) => {
 })
 
 // get single user including associated orders
-router.get('/:userId', async (req, res, next) => {
+router.get('/:userId', checkAdmin, async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.userId, {
       include: {
         model: Order
       }
     })
+    handle404(user)
     res.status(200).json(user)
   } catch (err) {
     next(err)
---
+++ util/server.js
@@ -0,0 +1,23 @@
+const handle404 = (trueFalse, message, status) => {
+  if (!trueFalse) {
+    const err = new Error(message || 'Not Found')
+    err.status = status || 404
+    throw err
+  }
+}
+
+const checkAdmin = (req, res, next) => {
+  const user = req.session.user
+  if (user && user.isAdmin) {
+    next()
+  } else {
+    const err = new Error('Access Denied')
+    err.status = 401
+    next(err)
+  }
+}
+
+module.exports = {
+  handle404,
+  checkAdmin
+}
