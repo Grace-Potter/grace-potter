d3831f28ccd0804d1b445ebd2ad4ed478298ede9
Mike Koshakow <mzkosh@gmail.com>
2020-08-11 12:16:37 -0400

    implement pagination

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import ProductList from './ProductList'
+import {ProductList, Pagination} from './index'
 import {fetchProducts} from '../store/allProducts'
 import {Link} from 'react-router-dom'
 
 export class AllProducts extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      currentPage: 1,
+      itemsPerPage: 2,
+    }
+    this.paginate = this.paginate.bind(this)
+  }
+
   componentDidMount() {
     this.props.getProducts()
   }
 
+  paginate(pageNumber) {
+    this.setState({
+      currentPage: pageNumber,
+    })
+  }
+
   render() {
+    const indexOfLastPost = this.state.currentPage * this.state.itemsPerPage
+    const indexOfFirstPost = indexOfLastPost - this.state.itemsPerPage
+    const products = this.props.products.slice(
+      indexOfFirstPost,
+      indexOfLastPost
+    )
+
     return (
       <div>
         <header>
@@ -20,21 +42,23 @@ export class AllProducts extends React.Component {
             </Link>
           )}
         </header>
-        <ProductList
-          products={this.props.products}
-          fromPortal={this.props.fromPortal}
+        <ProductList products={products} fromPortal={this.props.fromPortal} />
+        <Pagination
+          itemsPerPage={this.state.itemsPerPage}
+          totalItems={this.props.products.length}
+          paginate={this.paginate}
         />
       </div>
     )
   }
 }
 
-const mapState = state => ({
-  products: state.allProducts
+const mapState = (state) => ({
+  products: state.allProducts,
 })
 
-const mapDispatch = dispatch => ({
-  getProducts: () => dispatch(fetchProducts())
+const mapDispatch = (dispatch) => ({
+  getProducts: () => dispatch(fetchProducts()),
 })
 
 export default connect(mapState, mapDispatch)(AllProducts)
